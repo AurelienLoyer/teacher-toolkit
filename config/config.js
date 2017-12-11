b625249ae520db8fdd7973b3b730e93539e4b12d
Aurélien Loyer <aur.loy@gmail.com>
2017-12-11 22:12:21 +0100

    feat(config): edit config file system

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BASE_URI: 'http://localhost:1337',
+  PORT: 1337,
+  ngrok_proto: 'http', // http|tcp|tls
+  ngrok_auth: '', // http basic authentication for tunnel
+  ngrok_subdomain: '', // reserved tunnel name https://alex.ngrok.io
+}
--- config/config.model.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  BASE_URI: 'http://localhost:1337', // 
-  PORT: 1337, // 
-  ngrok_proto: 'http', // http|tcp|tls
-  ngrok_auth: 'user:pwd', // http basic authentication for tunnel
-  ngrok_subdomain: 'aurel', // reserved tunnel name https://alex.ngrok.io
-}

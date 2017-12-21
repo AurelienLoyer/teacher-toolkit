58042baa8b7a9c7c5213710010caf1a1225d1c9b
Aurélien Loyer <aur.loy@gmail.com>
2017-12-21 17:42:36 +0100

    feat(front): create new component loader/error

---
+++ front/src/components/Error.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="error">
+        <img src="src/assets/error.png" alt="">
+        <strong>Server error :'(</strong>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.error {
+  background-color: #f7f7f7;
+  padding: 10px;
+  max-width: 220px;
+  margin: auto;
+  margin-top: 20px;
+  border-radius: 15px;
+  img {
+    height: 100px;
+    margin-bottom: 20px;
+  }
+  strong {
+    display: block;
+  }
+}
+</style>
---
+++ front/src/components/Loader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="loader">
+        <img src="src/assets/loader.gif" alt="">
+        <strong>Loading {{type}}...</strong>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'type',
+        ],
+    }
+</script>
+
+<style lang="scss" scoped>
+    .loader {
+        padding: 10px;
+        max-width: 220px;
+        margin: auto;
+        margin-top: 20px;
+        border-radius: 15px;
+        img {
+            height: 100px;
+        }
+        strong {
+            display: block;
+            margin-bottom: 20px;
+        }
+    }
+</style>
